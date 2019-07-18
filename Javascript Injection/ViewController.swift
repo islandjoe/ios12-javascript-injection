8bc84f90ce3a5f9df11762b547d805da60af88c4
Arthur Kho Caayon <islandjoe@gmail.com>
2019-07-18 10:18:58 +0300

    Initial Commit

---
+++ Javascript Injection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Javascript Injection
+//
+//  Created by Arthur Kho on 18/07/2019.
+//  Copyright © 2019 Arthur Kho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
